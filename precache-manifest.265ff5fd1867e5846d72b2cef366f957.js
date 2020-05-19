5c148ceb2e81907997e700d1ce6b7e34c9e594c1
PrithiveeK <prithivee@codingmart.com>
2020-05-19 12:41:04 +0530

    Updates

---
+++ precache-manifest.265ff5fd1867e5846d72b2cef366f957.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "138d69c979d124f38fc8ee5f1af30d08",
+    "url": "/ShoppingApp/index.html"
+  },
+  {
+    "revision": "1e1d3ec08f21a76b61da",
+    "url": "/ShoppingApp/static/css/main.53c5aa57.chunk.css"
+  },
+  {
+    "revision": "32a081cc0b0293cdbe9e",
+    "url": "/ShoppingApp/static/js/2.dbbe9196.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ShoppingApp/static/js/2.dbbe9196.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e1d3ec08f21a76b61da",
+    "url": "/ShoppingApp/static/js/main.19e3ccc3.chunk.js"
+  },
+  {
+    "revision": "9b6ecaad1aa895e09134",
+    "url": "/ShoppingApp/static/js/runtime-main.56303ccf.js"
+  }
+]);
