5c9b6027a112a103d0bca22485cc3e68374dfbbd
PrithiveeK <prithivee@codingmart.com>
2020-05-19 12:49:22 +0530

    Updated routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { HashRouter, Route} from 'react-router-dom';
 import Header from './components/Header';
 import ShoppingList from './components/ShoppingList';
 import NewProduct from './components/NewProduct';
 
 function App() {
   return (
-    <Router>
+    <HashRouter basename='/'>
     <div className="App">
       <Header />
       <Route path="/home" component={ShoppingList} />
       <Route path="/admin" component={NewProduct} />
     </div>
-    </Router>
+    </HashRouter>
   );
 }
 
